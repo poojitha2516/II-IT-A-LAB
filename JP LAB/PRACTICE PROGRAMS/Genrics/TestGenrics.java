514af4d0aaf353ee4e93ec141805e77c8fad5c04
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 14:43:32 +0530

    Create TestGenrics.java

---
+++ JP LAB/PRACTICE PROGRAMS/Genrics/TestGenrics.java
@@ -0,0 +1,18 @@
+// Generics
+
+import java.util.*;  
+class TestGenerics {  
+	public static void main(String args[]) {
+		ArrayList<String> list=new ArrayList<String>();  
+        list.add("Penmetsa");  
+        list.add("Poojitha");  
+  
+        String s=list.get(1);//type casting is not required  
+        System.out.println("element is: "+s);  
+  
+        Iterator<String> itr=list.iterator();  
+        while(itr.hasNext()) {   
+			System.out.println(itr.next());  
+        }  
+    }  
+}  
