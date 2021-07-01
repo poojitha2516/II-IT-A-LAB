91b36994a0b309969e855822063e1e1412c5189c
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 17:57:09 +0530

    Create StringReplaceMethod.java

---
+++ JP LAB/PRACTICE PROGRAMS/String and String Methods/StringReplaceMethod.java
@@ -0,0 +1,10 @@
+// String mathods : replace method
+
+public class Example4 {
+	public static void main(String args[]) {
+		String s1 = "My name is Pinky";
+		
+		System.out.println(s1.replace('P','B'));
+		
+	}
+}
