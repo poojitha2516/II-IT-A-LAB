c5d273d080b5bdfc376622f40a408c0f32e29b31
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 17:56:32 +0530

    Create StringLengthMethod.java

---
+++ JP LAB/PRACTICE PROGRAMS/String and String Methods/StringLengthMethod.java
@@ -0,0 +1,13 @@
+// String method : Length method
+
+public class Example3 {
+	public static void main(String args[]) {
+		String s1 = "BVRIT";
+		String s2 = "Hyderabad";
+		
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		//Another way 
+		System.out.println("IT Department".length());
+	}
+}
