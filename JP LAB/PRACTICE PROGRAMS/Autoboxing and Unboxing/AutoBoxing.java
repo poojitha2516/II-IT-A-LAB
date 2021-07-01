b0946450d1d375795050d68833e9ba0f5d7322ab
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 10:45:08 +0530

    Create AutoBoxing.java

---
+++ JP LAB/PRACTICE PROGRAMS/Autoboxing and Unboxing/AutoBoxing.java
@@ -0,0 +1,12 @@
+// AutoBoxing 
+
+class AutoBoxing {
+	public static void main(String args[]) {
+		int i = 20;
+		Integer i2 = new Integer(i); // boxing
+		Integer i3 = 10; // Boxing
+		
+		System.out.println("i2 = "+i2);
+		System.out.println("i3 = "+i3);
+	}
+}
