6bc7619fe092c531dfb2c6329df62862b737a388
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 17:58:11 +0530

    Create StringTrimMethod.java

---
+++ JP LAB/PRACTICE PROGRAMS/String and String Methods/StringTrimMethod.java
@@ -0,0 +1,11 @@
+// String method : trim method
+
+public class Example6 {
+	public static void main(String[] args) {
+		String s1 = "       Hello         ";
+		String s2 = "  Java";
+		
+		System.out.println(s1.trim());
+		System.out.println(s2.trim());
+	}
+}
