737e053f8310e942898ac63e28cb03144eefafd1
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 15:23:30 +0530

    Create DoWhileLoop.java

---
+++ JP LAB/PRACTICE PROGRAMS/Loops/DoWhileLoop.java
@@ -0,0 +1,11 @@
+// do while loop
+
+public class Loop4 {
+	public static void main(String args[]) {
+		int x = 1;
+		do {
+			System.out.println("Value of x : "+ x);
+			x++;
+		} while(x<3);
+	}
+}
