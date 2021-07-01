d0d1613e65e799f4205740fa1941734d1100f505
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 10:49:05 +0530

    Create DefaultConstructor.java

---
+++ JP LAB/PRACTICE PROGRAMS/Constructors/DefaultConstructor.java
@@ -0,0 +1,11 @@
+// Default Constructor
+
+public class Color {
+	Color() {   // Default Constructor is created
+		System.out.println("My Favourite color is Blue ...");
+	}
+	public static void main(String args[]) {
+		Color c = new Color();  // Calling a default constructor
+	}
+	
+}
