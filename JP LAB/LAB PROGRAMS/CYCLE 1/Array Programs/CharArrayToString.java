c2debdb36bceae4080cda9e2877fd0d4c53340e5
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 09:39:11 +0530

    Create CharArrayToString.java

---
+++ JP LAB/LAB PROGRAMS/CYCLE 1/Array Programs/CharArrayToString.java
@@ -0,0 +1,9 @@
+// Java Program to convert char Array to String
+
+public class Chararraytostring {
+	public static void main(String args[]) {
+		char[] ch = {'P','o','o','j','i','t','h','a'};
+		String s = new String(ch);
+		System.out.println(s);
+	}
+}
