deda64963042c9b220c9b3f6c31c461e107d820d
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 14:16:35 +0530

    Create Throw.java

---
+++ JP LAB/PRACTICE PROGRAMS/Exception Handling/Throw.java
@@ -0,0 +1,11 @@
+// Exception Handling : throw
+
+class Excep4 {
+  public static void display() {
+	 throw new ArithmeticException("Trying to do divide by zero");
+  }
+
+  public static void main(String[] args) {
+	  display();
+  }
+}
