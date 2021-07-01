c295b9e6c26ddc33a2b89f3a9ea971484cf2ebc4
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 18:05:16 +0530

    Create Packages.java

---
+++ JP LAB/PRACTICE PROGRAMS/mypack/Packages.java
@@ -0,0 +1,23 @@
+// Saved as Z.java
+//package mypack ()
+
+import pack.*;
+class Z {
+	public static void main(String args[]) {
+		Y y = new Y();
+		y.show();
+	}
+}
+
+
+
+//Saved by Y.java
+
+package pack;
+public class Y {
+	public void show() {
+		System.out.println("Hello World !");
+	}
+}
+
+
