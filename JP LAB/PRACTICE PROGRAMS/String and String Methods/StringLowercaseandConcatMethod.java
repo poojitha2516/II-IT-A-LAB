2cb9e719a358387e962f1d2b31c7e87043106799
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 17:59:09 +0530

    Create StringLowercaseandConcatMethod.java

---
+++ JP LAB/PRACTICE PROGRAMS/String and String Methods/StringLowercaseandConcatMethod.java
@@ -0,0 +1,12 @@
+// String methods : lowercase method,concat method
+
+public class Example7 {
+	public static void main(String args[]) {
+		String s1 = "BVRIT";
+		String s2 = "HYDERABAD";
+		System.out.println(s1.toLowerCase()); // lowercase method
+		System.out.println(s2.toLowerCase());
+		
+		System.out.println(s1.concat(s2)); // concat method
+	}
+}
