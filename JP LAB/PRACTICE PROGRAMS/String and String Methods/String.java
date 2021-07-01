21a5144c5cf9d8c590fa18ab4b7b02bbc7aa1fa2
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-01 17:54:42 +0530

    Create String.java

---
+++ JP LAB/PRACTICE PROGRAMS/String and String Methods/String.java
@@ -0,0 +1,11 @@
+// String
+
+public class Example1 {
+	public static void main(String[] args) {
+		String s1= "I am from IT department";
+		String s2 = new String("IInd year");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
