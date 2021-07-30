43f60f1de4cd0cc212e7a50b733966a950f91207
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-07-30 10:13:49 +0530

    Create Mousetest.java

---
+++ JP LAB/LAB PROGRAMS/CYCLE 3/Mousetest.java
@@ -0,0 +1,49 @@
+/* Write a Java program that handles all mouse events and shows the event name at the center of the window
+when a mouse event is fired (Use Adapter classes). */
+
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+class MouseEvents extends JFrame implements MouseListener {
+	JLabel l1;
+	public MouseEvents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,400);
+		setLayout(new FlowLayout());
+		l1 = new JLabel();
+		Font f = new Font("Verdana",Font.BOLD,20);
+		l1.setFont(f);
+		l1.setForeground(Color.BLUE);
+		add(l1);
+		addMouseListener(this);
+		setVisible(true);
+	}
+	
+	public void mouseExited(MouseEvent m) {
+		l1.setText("Mouse Exited");
+	}
+	
+	public void mouseEntered(MouseEvent m) {
+		l1.setText("Mouse Entered");
+	}
+	
+	public void mouseReleased(MouseEvent m) {
+		l1.setText("Mouse Released");
+	}
+	
+	public void mousePressed(MouseEvent m) {
+		l1.setText("Mouse Pressed");
+	}
+	
+	public void mouseClicked(MouseEvent m) {
+		l1.setText("Mouse Clicked");
+	}
+}
+
+public class Mousetest {
+	public static void main(String args[]) {
+		MouseEvents a = new MouseEvents();
+	}
+}
