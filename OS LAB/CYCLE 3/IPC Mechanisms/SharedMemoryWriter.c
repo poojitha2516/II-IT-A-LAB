8d53d233d6e86b4e8fb655a92de75195340605aa
poojitha <65111786+poojitha2516@users.noreply.github.com>
2021-08-09 19:46:19 +0530

    Create SharedMemoryWriter.c

---
+++ OS LAB/CYCLE 3/IPC Mechanisms/SharedMemoryWriter.c
@@ -0,0 +1,21 @@
+/* Write C programs to illustrate the following IPC mechanisms
+a) Pipes b) FIFOs c) Message Queues d) Shared Memory */
+
+// d) Shared Memory
+//  writer process 
+
+
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<stdio.h>
+int main()
+{
+        key_t key = ftok("shmfile",65);
+        int shmid = shmget(key,1024,0666|IPC_CREAT);
+        char *str = (char*) shmat(shmid,(void*)0,0);
+        printf("Write Data : ");
+        fgets(str,50,stdin);
+        printf("Data written in memory: %s\n",str);
+        shmdt(str);
+        return 0;
+}
